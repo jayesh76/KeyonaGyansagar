98b40750f3442a1dfd4e74f98a5b3cf3e3899579
jayesh <jbsolanki@gmail.com>
2021-02-18 15:32:40 +0530

    user table changes

--- src/main/java/io/keyona/gyansagar/domain/User.java
+++ src/main/java/io/keyona/gyansagar/domain/User.java
@@ -13,6 +13,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
+@Table(name="keyonauser")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
